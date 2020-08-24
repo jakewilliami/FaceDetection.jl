48bbfa725af4675680399c2a7a6fe1dd28a04c19
Jake W. Ireland <jakewilliami@icloud.com>
2020-08-25 10:43:19 +1200

    Added other dataset

--- setup.sh
+++ setup.sh
@@ -10,52 +10,114 @@ trap "exit" INT
 echo "Please ensure FaceDetection.jl is installed in your home directory..."
 sleep 5
 
-mkdir -p ~/FaceDetection.jl/test/images/testing/pos/
-mkdir -p ~/FaceDetection.jl/test/images/testing/neg/
-mkdir -p ~/FaceDetection.jl/test/images/testing/testing/pos/
-mkdir -p ~/FaceDetection.jl/test/images/testing/testing/neg/
+FD_HOME="${HOME}/FaceDetection.jl/"
+MAIN="${FD_HOME}/data/images/"
+ALT="${FD_HOME}/data/alt/"
 
-brew list > /tmp/brewlist
-if ! grep "^imagemagick$" /tmp/brewlist > /dev/null 2>&1
-then
-	echo "Downloading ImageMagick"
-	brew install imagemagick
-fi
-if ! grep "^julia$" /tmp/brewlist > /dev/null 2>&1
-then
-	echo "Downloading Julia"
-	brew install julia
-fi
 
-echo "Downloading dependencies"
-julia -E 'import Pkg; Pkg.activate(joinpath(homedir(), "FaceDetection.jl")); Pkg.instantiate()'
+setupWD() {
+	mkdir -p ${MAIN}/testing/pos/
+	mkdir -p ${MAIN}/testing/neg/
+	mkdir -p ${MAIN}/testing/testing/pos/
+	mkdir -p ${MAIN}/testing/testing/neg/
+}
 
-echo "Downloading face detection training data"
-if [[ -d ~/FaceDetection.jl/test/images/pos/ ]]
-then
-	rm -rf ~/FaceDetection.jl/test/images/pos/
-fi
-if [[ -d ~/FaceDetection.jl/test/images/neg/ ]]
-then
-	rm -rf ~/FaceDetection.jl/test/images/neg/
-fi
-cd ~/FaceDetection.jl/ && \
-	git clone https://github.com/OlegTheCat/face-detection-data && \
-	mv face-detection-data/pos/ test/images/ && \
-	mv face-detection-data/neg/ test/images/ && \
-	rm -rf face-detection-data/
-echo "Pruning the positive training images to have the same number as the negative images, or else there will be an array mismatch when constructing the image array in src/Adaboost.jl"
-for i in $(seq $(ls ~/FaceDetection.jl/test/images/neg/ | wc -l) $(($(ls ~/FaceDetection.jl/test/images/pos/ | wc -l)-1)); do
-	rm ~/FaceDetection.jl/test/images/pos/${i}.pgm
-	# echo "${i}"
-done
 
+checkPackages() {
+	brew list > /tmp/brewlist
+	if ! grep "^imagemagick$" /tmp/brewlist > /dev/null 2>&1
+	then
+		echo "Downloading ImageMagick"
+		brew install imagemagick
+	fi
+	if ! grep "^julia$" /tmp/brewlist > /dev/null 2>&1
+	then
+		echo "Downloading Julia"
+		brew install julia
+	fi
+
+	echo "Downloading dependencies"
+	julia -E 'import Pkg; Pkg.activate(joinpath(homedir(), "FaceDetection.jl")); Pkg.instantiate()'
+}
+
+
+obtainDataset1() {
+	echo "Downloading face detection training data"
+	if [[ -d ${MAIN}/pos/ ]]
+	then
+		rm -rf ${MAIN}/pos/
+	fi
+	if [[ -d ${MAIN}/neg/ ]]
+	then
+		rm -rf ${MAIN}/neg/
+	fi
+	cd ${FD_HOME}/ && \
+		git clone https://github.com/OlegTheCat/face-detection-data && \
+		mv ${FD_HOME}/face-detection-data/pos/ ${MAIN}/ && \
+		mv ${FD_HOME}/face-detection-data/neg/ ${MAIN}/ && \
+		rm -rf ${FD_HOME}/face-detection-data/
+	echo "Pruning the positive training images to have the same number as the negative images, or else there will be an array mismatch when constructing the image array in src/Adaboost.jl"
+	for i in $(seq $(ls ${MAIN}/neg/ | wc -l) $(($(ls ${MAIN}/pos/ | wc -l)-1))); do
+		rm ${MAIN}/pos/${i}.pgm
+		# echo "${i}"
+	done
+}
+
+
+obtainDataset2() {
+	echo "Dowloading alternative training data"
+	if [[ -d ${FD_HOME}/Viola-Jones/ ]]
+	then
+		rm -rf ${FD_HOME}/Viola-Jones/
+	fi
+	if [[ -d ${ALT}/testset/ ]]
+	then
+		rm -rf ${ALT}/testset/
+	fi
+	if [[ -d ${ALT}/trainset/ ]]
+	then
+		rm -rf ${ALT}/trainset/
+	fi
+	mkdir -p ${ALT}/
+	cd ${FD_HOME}/ && \
+		git clone https://github.com/INVASIS/Viola-Jones/ && \
+		mv ${FD_HOME}/Viola-Jones/data/testset/ ${ALT}/ && \
+		mv ${FD_HOME}/Viola-Jones/data/trainset/ ${ALT}/ && \
+		rm -rf ${FD_HOME}/Viola-Jones
+	echo "Pruning the positive training images to have the same number as the negative images, or else there will be an array mismatch when constructing the image array in src/Adaboost.jl"
+	# for i in $(seq $(ls ${ALT}/trainset/faces | wc -l) $(($(ls ~/FaceDetection.jl/test/images/pos/ | wc -l)-1))); do
+	# 	rm ${ALT}/trainset/non-faces/${i}.pgm
+	# 	# echo "${i}"
+	# done
 	
+	find ${ALT}/trainset/non-faces/ -maxdepth 1 -type f -name "*.png" -print | \
+        head -n $(($(ls ${ALT}/trainset/non-faces | wc -l)-$(ls ${ALT}/trainset/faces | wc -l))) |\
+		while IFS= read -r file
+		do
+			rm "${file}"
+		done
+}
+
+
+convertPGM() {
 #### The following step was only for python testing code.  Netpbm.jl is powerful and fixed this.
 
-# echo "Converting pgm files to png.  This will take a minute."
-# find ../test/images/ -name "*.pgm" | \
-# while IFS= read -r file; do
-# 	magick "${file}" "${file}.png"
-# done
-	
+echo "Converting pgm files to png.  This will take a minute."
+find ../test/images/ -name "*.pgm" | \
+while IFS= read -r file; do
+	magick "${file}" "${file}.png"
+done
+}
+
+
+
+
+main() {
+	setupWD
+	checkPackages
+	obtainDataset1
+	obtainDataset2
+}
+
+
+main
