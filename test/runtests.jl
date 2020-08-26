68ad3eddfceb8acab1a73661c6c3e025c8736db7
Jake W. Ireland <jakewilliami@icloud.com>
2020-08-27 00:23:19 +1200

    added beginnings of testing package (touches on good practices regarding #8)

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+    #=
+    exec julia --project="~/FaceDetection.jl/" "${BASH_SOURCE[0]}" "$@" -e "include(popfirst!(ARGS))" \
+    "${BASH_SOURCE[0]}" "$@"
+    =#
+
+using Test
+using FaceDetection
+
+# @test ... == ...
