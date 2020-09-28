235357fe50eefd688187f38a9994898404d07211
Jake W. Ireland <jakewilliami@icloud.com>
2020-09-28 20:05:19 +1300

    Added test-only and example-only dependencies (removing example- and test-specific dependencies from main project file)

--- examples/roc.jl
+++ examples/roc.jl
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
     #=
-    exec julia --project="$(realpath $(dirname $(realpath $(dirname $0))))/" "${BASH_SOURCE[0]}" "$@" -e "include(popfirst!(ARGS))" \
+    exec julia --project="$(realpath $(dirname $0))/" "${BASH_SOURCE[0]}" "$@" -e "include(popfirst!(ARGS))" \
     "${BASH_SOURCE[0]}" "$@"
     =#
--- examples/scores.jl
+++ examples/scores.jl
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
     #=
-    exec julia --project="$(realpath $(dirname $(realpath $(dirname $0))))/" "${BASH_SOURCE[0]}" "$@" -e "include(popfirst!(ARGS))" \
+    exec julia --project="$(realpath $(dirname $0))/" "${BASH_SOURCE[0]}" "$@" -e "include(popfirst!(ARGS))" \
     "${BASH_SOURCE[0]}" "$@"
     =#
 
