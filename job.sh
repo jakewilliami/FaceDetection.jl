c5d64f3253fd37c65ce18992c960affd9e3fb0fe
Jake W. Ireland <jakewilliami@icloud.com>
2020-09-30 17:48:07 +1300

    Removed julia as files are already executable

--- job.sh
+++ job.sh
@@ -11,43 +11,43 @@
 EXAMPLE_DIR="${HOME}/projects/FaceDetection.jl/examples/"
 
 sed 's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 2/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 3/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 4/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 5/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 6/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 7/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 8/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 9/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 10/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 20/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 50/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 100/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 200/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
 
 sed	's/^num_classifiers[[:space:]]=[[:space:]].*$/num_classifiers = 1000/g' "${EXAMPLE_DIR}/constants.jl"
-julia "${EXAMPLE_DIR}/write.jl"
+${EXAMPLE_DIR}/write.jl
