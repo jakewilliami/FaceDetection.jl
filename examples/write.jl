4493b30550e4456b9e2f2d349fecd3d9d4b3736e
Jake W. Ireland <jakewilliami@icloud.com>
2020-10-19 16:15:53 +1300

    Made write.jl use updated process for efficiency of memory

--- examples/write.jl
+++ examples/write.jl
@@ -33,20 +33,23 @@ function main(;
 		include("alt_data.jl")
 	end
 
-    min_size_img = (19, 19) # default for our test dataset
-    if smart_choose_feats
-        # For performance reasons restricting feature size
-        notify_user("Selecting best feature width and height...")
-        
-        max_feature_width, max_feature_height, min_feature_height, min_feature_width, min_size_img = determine_feature_size(pos_training_path, neg_training_path)
-        
-        println("...done.  Maximum feature width selected is $max_feature_width pixels; minimum feature width is $min_feature_width; maximum feature height is $max_feature_height pixels; minimum feature height is $min_feature_height.\n")
-    else
-        min_feature_height = 8
-        max_feature_height = 10
-        min_feature_width = 8
-        max_feature_width = 10
-    end
+	min_size_img = (19, 19) # default for our test dataset
+	if smart_choose_feats
+		# For performance reasons restricting feature size
+		notify_user("Selecting best feature width and height...")
+		
+		max_feature_width, max_feature_height, min_feature_height, min_feature_width, min_size_img = determine_feature_size(pos_training_path, neg_training_path; scale_up = true)
+		
+		println("...done.  Maximum feature width selected is $max_feature_width pixels; minimum feature width is $min_feature_width; maximum feature height is $max_feature_height pixels; minimum feature height is $min_feature_height.\n")
+	else
+		min_feature_height = 8
+		max_feature_height = 10
+		min_feature_width = 8
+		max_feature_width = 10
+	end
+
+	# classifiers are haar like features
+	classifiers = FD.learn(pos_training_path, neg_training_path, num_classifiers, min_feature_height, max_feature_height, min_feature_width, max_feature_width; scale_up = true)
 
 
     FD.notify_user("Loading faces...")
