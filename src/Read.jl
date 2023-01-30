314a3a04bf2706081882fbfa0abb238c709e8df3
Jake Ireland <jakewilliami@icloud.com>
2023-01-30 18:55:24 +1300

    Start work on read function

---
+++ src/Read.jl
@@ -0,0 +1,13 @@
+using LightXML
+
+# Based on:
+# https://github.com/BehindTheSciences/Face-Recognition-using-Matlab-and-Sift/blob/fef02a5f9e9fa643cb98d5db7a537be01a214927/ConvertHaarcasadeXMLOpenCV.m
+# And:
+# https://github.com/aldebaro/ufpa-face-detection/blob/d643708b9c338f4bf2c69f02d948c52f774bbb44/ufd_convertXML.m
+#
+# Adapted by Jake W. Ireland (November, 2022)
+# Originally written by D.Kroon, University of Twente (November, 2010)
+# With additional resource from Bruno Ricardo Scherer (February, 2016)
+function read_opencv_xml(datafile::AbstractString)
+
+end
